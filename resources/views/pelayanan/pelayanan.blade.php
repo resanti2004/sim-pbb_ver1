7a09863b6230b54420499cd95b90eefa08283d24
LENOVO <annisaurohmah@mail.ugm.ac.id>
2023-11-30 13:56:13 +0700

    view pelayanan

--- resources/views/pelayanan/pelayanan.blade.php
+++ resources/views/pelayanan/pelayanan.blade.php
@@ -2,86 +2,103 @@
 @section('title', 'Pelayanan')
 @section('content')
 
-			<div class="bgn">
-				<h1 class="title">Selamat Datang, {{$fullname}}!</h1>
-				<p class="greet">Selamat Datang di Sistem Informasi Pajak Bumi Bangunan</p>
-			</div>
+<div class="bgn">
+	<h1 class="title">Selamat Datang, {{$fullname}}!</h1>
+	<p class="greet">Selamat Datang di Sistem Informasi Pajak Bumi Bangunan</p>
+</div>
 
-			<div class="data">
-				<div class="box-container">
-				<div class="detail">
-					<div class="recentOrders">
-						<div class="cardHeader">
-							<a href=""><h3>Pelayanan</h3></a> 
-							<!-- <div aria-label="breadcrumb">
+<div class="data">
+	<div class="box-container">
+		<div class="detail">
+			<div class="recentOrders">
+				<div class="cardHeader">
+					<a href="">
+						<h3>Pelayanan</h3>
+					</a>
+					<!-- <div aria-label="breadcrumb">
 							<ol class="breadcrumb">
 							<li class="breadcrumb-item"><a href="#">Beranda</a></li>
 							<li class="breadcrumb-item active" aria-current="page">Pelayanan</li>
 							</ol>
 							</div> -->
-							<div class=" p-0 d-flex align-items-start" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="#">Beranda</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Pelayanan</li>
-                                </ol>
-                            </div>
-						</div>
-						<!-- <form action="#">
+					<div class=" p-0 d-flex align-items-start" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item"><a href="#">Beranda</a></li>
+							<li class="breadcrumb-item active" aria-current="page">Pelayanan</li>
+						</ol>
+					</div>
+				</div>
+				<!-- <form action="#">
 								<div class="form-group">
 									<input type="text" placeholder="Search...">
 									<i class='bx bx-search icon' ></i>
 								</div>
 						</form> -->
-						<div class="pencarian">
-							<a href="{{ route('pelayananLap.index')}}"><button class="bg-success" type="button">Laporan Pelayanan</button></a>
-							<a href="{{ route('pelayanan.create') }}"><button type="button">+ Buat Baru</button></a>
-						</div>
-						<table>
-							<thead>
-								<tr>
-									<td>No</td>
-									<td>No Pelayanan</td>
-									<td>Nama Pemohon</td>
-									<td>Tanggal Pelayanan</td>
-									<td>Kecamatan</td>
-									<td>Kelurahan</td>
-									<td>Blok</td>
-									<td>No Urut</td>
-									<td>Kode Jenis Pelayanan</td>
-									<td>Status Pelayanan</td>
-									<td>Opsi</td>
-								</tr>
-							</thead>
-
-							<tbody>
-								
-								<tr>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td>
-										<ul class="list-inline">									
-											<li class="list-inline-item"><a href="{{ route('pelayanan.show') }}" class="active"><i class='bx bxs-show' ></i></a></li>
-											<li class="list-inline-item"><a href="#" class="active"><i class='bx bxs-edit' ></i></a></li>
-											<li class="list-inline-item"><a href="#" class="active"><i class='bx bxs-trash' ></i></a></li>
-										</ul>
-									</td>
-								</tr>
-								
-							</tbody>
-						</table>
-						<div class="d-flex justify-content-center">
-							
-						</div>
-						
-					</div>
+				<div class="pencarian">
+					<a href="{{ route('pelayananLap.index')}}"><button class="bg-success" type="button">Laporan Pelayanan</button></a>
+					<a href="{{ route('pelayanan.create') }}"><button type="button">+ Buat Baru</button></a>
 				</div>
+				<table>
+					<thead>
+						<tr>
+							<td>No</td>
+							<td>No Pelayanan</td>
+							<td>Nama Pemohon</td>
+							<td>Tanggal Pelayanan</td>
+							<td>Kecamatan</td>
+							<td>Kelurahan</td>
+							<td>Blok</td>
+							<td>No Urut</td>
+							<td>Kode Jenis Pelayanan</td>
+							<td>Status Pelayanan</td>
+							<td>Keterangan Berkas</td>
+							<td>Opsi</td>
+						</tr>
+					</thead>
+
+					<tbody>
+						@foreach($data_pelayanan as $pelayanan)
+						<tr>
+							<td>{{ $no++ }} </td>
+							<td>{{ $pelayanan->NO_PELAYANAN }}</td>
+							<td>{{ $pelayanan->NAMA_PEMOHON }}</td>
+							<td>{{ $pelayanan->TANGGAL_PELAYANAN }}</td>
+							<td>{{ $pelayanan->KECAMATAN }}</td>
+							<td>{{ $pelayanan->KELURAHAN}}</td>
+							<td>{{ $pelayanan->KD_BLOK }}</td>
+							<td>{{ $pelayanan->NO_URUT }}</td>
+							<td><?php
+								$kd = \App\Models\RefJnsPelayanan::all()->pluck('NM_JENIS_PELAYANAN', 'KD_JNS_PELAYANAN')->toArray();
+								echo $kd[$pelayanan->KD_JNS_PELAYANAN] ?? '';
+								?>
+							</td>
+							<td>
+								<?php
+								$status = \App\Models\StatusPelayanan::orderBy('urutan')->pluck('nama', 'id')->toArray();
+								if (!isset($pelayanan->STATUS_PELAYANAN)) {
+									echo $status[1];
+								} else {
+									echo $status[$pelayanan->STATUS_PELAYANAN];
+								}
+								?>
+							</td>
+							<td>{{ $pelayanan->KETERANGAN_BERKAS }}</td>
+							<td>
+								<ul class="list-inline">
+									<li class="list-inline-item"><a href="{{ route('pelayanan.show') }}" class="active"><i class='bx bxs-show'></i></a></li>
+									<li class="list-inline-item"><a href="#" class="active"><i class='bx bxs-edit'></i></a></li>
+									<li class="list-inline-item"><a href="#" class="active"><i class='bx bxs-trash'></i></a></li>
+								</ul>
+							</td>
+						</tr>
+						@endforeach
+					</tbody>
+				</table>
+				<div class="d-flex justify-content-center">
+					{{ $data_pelayanan->links() }}
+				</div>
+
 			</div>
-@endsection
+		</div>
+	</div>
+	@endsection
