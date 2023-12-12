1f7b7b09e6c2d99f40b1b8fc3a5e191510f663a2
LENOVO <annisaurohmah@mail.ugm.ac.id>
2023-12-12 16:34:34 +0700

    revisi dikit

--- app/Http/Controllers/TunggakanController.php
+++ app/Http/Controllers/TunggakanController.php
@@ -98,6 +98,9 @@ class TunggakanController extends Controller
             ])->with('layout', 'report');
         }
 
-        return view('keuangan.tunggakan');
+        return redirect()->route('tunggakan.index', [
+            'fullname' => $fullname,
+            'username' => $username,
+        ]);
     }
 }
